d1ce4a89b2835b5778f65286a9cba6abfd11d825
finaxis002 <finaxis.ai@gmail.com>
2025-06-06 16:27:03 +0530

    case slice

--- src/features/caseSlice.ts
+++ src/features/caseSlice.ts
@@ -71,7 +71,7 @@ export const updateCase = createAsyncThunk(
       const token = localStorage.getItem("token"); // get token from localStorage
 
       const response = await axios.put(
-        `http://localhost:3000/api/cases/${caseData.id}`,
+        `https://tumbledrybe.sharda.co.in/api/cases/${caseData.id}`,
         caseData,
         {
           headers: {
