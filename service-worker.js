38339f34ff476892766f15c17b88906ee3c50206
finaxis002 <finaxis.ai@gmail.com>
2025-06-12 12:22:04 +0530

    removed cache all

--- service-worker.js
+++ service-worker.js
@@ -2,19 +2,19 @@
 // import icon as "./assets/favicon"
 
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("static-v1").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/app.js", // Add other static assets like JS and CSS files you need
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open("static-v1").then((cache) => {
+//       return cache.addAll([
+//         "/",
+//         "/index.html",
+//         "/style.css",
+//         "/app.js", // Add other static assets like JS and CSS files you need
        
-      ]);
-    })
-  );
-});
+//       ]);
+//     })
+//   );
+// });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
