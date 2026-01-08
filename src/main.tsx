86e3fa2cdae59a0760ea49b19feeac0e56edcf66
finaxis <finaxis.ai@gmail.com>
2026-01-08 11:10:15 +0530

    fixes

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,16 @@ import { store } from "./store";
 import { AppNameProvider } from "./contenxt/AppNameContext";
 import { AuthProvider } from "./contenxt/AuthProvider";
 
+// Unregister any existing service worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.unregister();
+    });
+  });
+}
+
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
     <Provider store={store}>
